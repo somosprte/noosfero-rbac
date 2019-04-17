17e7384ce0c5c10d9075cc8b96909a4cf30ffbd0
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-04-17 00:50:23 -0300

    show location on map only selected public option

--- plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
+++ plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
@@ -3,10 +3,10 @@ class NetworkMapPluginMapController < PublicController
     def index
         @query = params[:query]
         @type = params[:type] || :people
-        # @searches = {map: {results: environment.send(@type)}}
-        @scope = environment.send(@type).accessible_to(user).visible
+        @scope = environment.send(@type).accessible_to(user)
         @searches = {}
-        @searches[@type] = find_by_contents(@type, environment, @scope, @query)
+        @searches[@type] = find_by_contents(@type, environment, @scope, @query, {page: 1, per_page: @scope.count})
+        @searches[@type][:results] = @searches[@type][:results].select { |profile| profile.fields_privacy[:location] == "public" }
         @types = [
             [_('People'), :people],
             [_('Enterprises'), :enterprises],
