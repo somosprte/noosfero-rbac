eb9de148a9c21f58f8c9f5339637a30bb21e0b96
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-05-22 21:45:40 -0300

    fix functions method

--- plugins/activity_gallery/lib/activity_gallery_plugin/helper.rb
+++ plugins/activity_gallery/lib/activity_gallery_plugin/helper.rb
@@ -1,6 +1,6 @@
 module ActivityGalleryPlugin::Helper
 
-    def activity_actions(activity)
+    def activity_actions
         [
             link_to(font_awesome(:edit, _('Edit')), '#'),
             link_to(font_awesome(:trash, _('Remove')), {controller: 'activity_gallery_plugin_activity', action: 'destroy', id: params[:id]}, {data: {confirm: _('Você tem certeza que deseja excluir essa atividade?')}}),
