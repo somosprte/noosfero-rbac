03ed772b8712bd9aa2f3a4e18dc76605a6dc8a64
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-06-19 11:47:48 -0300

    show 4 activities per page

--- plugins/activity_gallery/controllers/profile/activity_gallery_plugin_activity_controller.rb
+++ plugins/activity_gallery/controllers/profile/activity_gallery_plugin_activity_controller.rb
@@ -8,7 +8,7 @@ class ActivityGalleryPluginActivityController < ProfileController
     def index
         @activities = get("gallery/v1/activities/#{build_search_params}").
             map { |activity| ActivityGalleryPlugin::Activity.new(activity) }
-        @results = @activities.paginate(page: params[:page] || 1, per_page: 6)
+        @results = @activities.paginate(page: params[:page] || 1, per_page: 4)
     end
 
     def saved_activities
