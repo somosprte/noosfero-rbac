d2637143ac515f65f9b289ec44735ec0da7de2ea
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-06-20 00:46:41 -0300

    mape icons and types search

--- plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
+++ plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
@@ -3,13 +3,16 @@ class NetworkMapPluginMapController < PublicController
     no_design_blocks
 
     def index
+        @enterprise_kinds = environment.kinds.where(type: 'Enterprise')
         @query = params[:query]
-        @type = params[:type] || :people
+        @type = params[:type] || :profiles
         @scope = environment.send(@type).accessible_to(user)
+        @scope = @scope.joins(:kinds).where("kinds.name = '#{params[:kind]}'") if params[:kind].present?
         @searches = {}
         @searches[@type] = find_by_contents(@type, environment, @scope, @query, {page: 1, per_page: @scope.count})
         @searches[@type][:results] = @searches[@type][:results].select { |profile| profile.fields_privacy[:location] == "public" }
         @types = [
+            [_('All'), :profiles],
             [_('People'), :people],
             [_('Enterprises'), :enterprises],
             [_('Communities'), :communities]
