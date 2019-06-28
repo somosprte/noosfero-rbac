d00567ae458197a79bdd4d938daca2fdd88dbd81
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-06-28 00:45:00 -0300

    refact network map filter plugin

--- plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
+++ plugins/network_map/controllers/public/network_map_plugin_map_controller.rb
@@ -1,21 +1,32 @@
 class NetworkMapPluginMapController < PublicController
     
     no_design_blocks
+    helper_method :filter_profiles
 
     def index
         @enterprise_kinds = environment.kinds.where(type: 'Enterprise')
+        @community_kinds = environment.kinds.where(type: 'Community')
         @query = params[:query]
-        @type = params[:type] || :profiles
-        @scope = environment.send(@type).accessible_to(user)
-        @scope = @scope.joins(:kinds).where("kinds.name = '#{params[:kind]}'") if params[:kind].present?
-        @searches = {}
-        @searches[@type] = find_by_contents(@type, environment, @scope, @query, {page: 1, per_page: @scope.count})
-        @searches[@type][:results] = @searches[@type][:results].select { |profile| profile.fields_privacy[:location] == "public" }
-        @types = [
-            [_('All'), :profiles],
-            [_('People'), :people],
-            [_('Enterprises'), :enterprises],
-            [_('Communities'), :communities]
-        ]
+        @types = params[:types] || []
+        filter_profiles(@query, @types)
     end
+
+    private
+
+    def filter_profiles(query, types)
+        @scope = environment.profiles.accessible_to(user).left_outer_joins(:kinds)
+        type_conditions = types.map do |type|
+            type, kind = type.split('_')
+            conditions = ["profiles.type = '#{type.singularize.camelize}'"]
+            conditions << "kinds.name = '#{kind}'" if kind.present?
+            "(#{conditions.join(' AND ')})"
+        end.join(' OR ')
+        @scope = @scope.where(type_conditions) if types.present?
+        @searches = {}
+        type = 'network_map_plugin'
+        @searches[type] = find_by_contents(type, environment, @scope, query, {page: 1, per_page: @scope.count})
+        @searches[type][:results] = @searches[type][:results].select { |profile| profile.fields_privacy[:location] == "public" }
+        return @searches[type][:results]
+    end
+
 end
