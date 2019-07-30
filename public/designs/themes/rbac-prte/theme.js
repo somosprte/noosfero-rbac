ac84762e4c0dfac5d479b48a3b23af74fc249061
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-07-30 17:54:06 -0300

    default brazilian nationality

--- public/designs/themes/rbac-prte/theme.js
+++ public/designs/themes/rbac-prte/theme.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
     .prependTo('#signup-form');
   $('.profile-country').val('BR');
 });
+
+$(document).ready(function() {
+  $('.action-account-signup #signup-form-profile .oauth-login')
+    .detach()
+    .prependTo('#signup-form');
+  $('.select-nationality').val('Brazilian');
+});
