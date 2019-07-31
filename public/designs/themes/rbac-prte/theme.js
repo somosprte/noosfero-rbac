937d7c8f9ff9c79327c20935db7017d9ece0be7b
Nivaldo Neto <nivaldojsneto@gmail.com>
2019-07-30 23:44:03 -0300

    change map js logic

--- public/designs/themes/rbac-prte/theme.js
+++ public/designs/themes/rbac-prte/theme.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $('.action-account-signup #signup-form-profile .oauth-login')
     .detach()
     .prependTo('#signup-form');
-  $('.profile-country').val('BR');
+    if (!$('.profile-country').val())
+      $('.profile-country').val('BR');
 });
 
 $(document).ready(function() {
